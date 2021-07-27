fb6e672855a1acd77f4db8a167af4b76196cfe74
Mario Beltrán Alarcón <belco90@gmail.com>
2021-07-27 12:57:16 +0200

    setup React preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:jest/recommended',
+    'plugin:testing-library/react',
     'prettier',
   ],
   parserOptions: {
